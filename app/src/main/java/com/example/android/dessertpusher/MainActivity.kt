ab44ab4a8f080e130a7e56d59d36263606f35d66
Lyla McBeath Fujiwara <lmf@google.com>
2018-07-12 21:14:18 -0700

    Step.05-Exercise-Implement-onSaveInstanceState

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -78,6 +78,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         // Setup dessertTimer, passing in the lifecycle
         dessertTimer = DessertTimer(this.lifecycle)
 
+        // TODO (03) Check here if the Bundle savedInstanceState is null. If it isn't, get the
+        // three values you saved and restore them: revenue, desserts sold and the timer's
+        // seconds count. Also make sure to show the correct image resource.
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -152,6 +156,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    // TODO (01) Add lifecycle callback methods for onSaveInstanceState and onRestoreInstanceState
+    // TODO (02) In onSaveInstanceState, put the revenue, dessertsSold and
+    // dessertTimer.secondsCount in the state Bundle
 
     /** Lifecycle Methods **/
     override fun onStart() {
